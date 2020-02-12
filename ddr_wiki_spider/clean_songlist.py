939d3827955d8eed89a17f43dcd766cd50f62e5b
MSoup <19704779+MSoup@users.noreply.github.com>
2020-02-12 21:59:38 +0900

    changed naming of output

--- ddr_wiki_spider/clean_songlist.py
+++ ddr_wiki_spider/clean_songlist.py
@@ -4,7 +4,7 @@ import json
 text = r"ddr_wiki_spider/songlist_detailed.json"
 
 with open(text, "r") as read_file:
-  with open (r"output", "a+") as write_file:
+  with open (r"output.txt", "a+") as write_file:
     song_list = json.load(read_file)
     for song in song_list[0:20]:
       write_file.writelines(f"{song['song']} \n")
