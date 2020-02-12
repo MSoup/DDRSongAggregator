0460b90b62ddd052c0be0be9b021adda470d366d
MSoup <charmanduh@hotmail.com>
2020-02-12 05:37:07 +0000

    cleaner json and filtering

--- ddr_wiki_spider/clean_songlist.py
+++ ddr_wiki_spider/clean_songlist.py
@@ -1,12 +1,12 @@
 #Takes the data and cleans it
 import json
 
-text = r"ddr_wiki_spider/ddr_songlist.json"
+text = r"ddr_wiki_spider/songlist_detailed.json"
 
 with open(text, "r") as read_file:
   with open (r"output", "a+") as write_file:
     song_list = json.load(read_file)
-    for song in song_list[0:10]:
+    for song in song_list[0:20]:
       write_file.writelines(f"{song['song']} \n")
 
 
